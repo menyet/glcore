e31b9cf88c525a68abf7339bd4078ea6b7607218
Unknown <Andrej.Mandli@erni.sk>
2018-12-24 23:21:45 +0100

    First commit

---
+++ Openglapp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Openglapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            C x;
+
+            x = null;
+        }
+    }
+
+    public class C
+    {
+        int _a;
+    }
+}
