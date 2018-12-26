da59bee29cf9b7a437ef640b4de6fda7dadc04a8
Unknown <Andrej.Mandli@erni.sk>
2018-12-26 22:04:58 +0100

    Texture and shader classes

---
+++ Openglapp/Shader.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics.OpenGL4;
+
+namespace OpenglApp
+{
+    public class Shader: IDisposable
+    {
+        int _handle;
+
+        public Shader(string vertPath, string fragPath)
+        {
+            int VertexShader;
+            int FragmentShader;
+
+            string VertexShaderSource = LoadSource(vertPath);
+
+            VertexShader = GL.CreateShader(ShaderType.VertexShader);
+
+            GL.ShaderSource(VertexShader, VertexShaderSource);
+
+            GL.CompileShader(VertexShader);
+
+            string infoLogVert = GL.GetShaderInfoLog(VertexShader);
+            if (infoLogVert != System.String.Empty)
+                System.Console.WriteLine(infoLogVert);
+
+            string FragmentShaderSource = LoadSource(fragPath);
+            FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(FragmentShader, FragmentShaderSource);
+            GL.CompileShader(FragmentShader);
+
+            string infoLogFrag = GL.GetShaderInfoLog(VertexShader);
+            if (infoLogFrag != System.String.Empty)
+                System.Console.WriteLine(infoLogFrag);
+
+            _handle = GL.CreateProgram();
+
+            GL.AttachShader(_handle, VertexShader);
+            GL.AttachShader(_handle, FragmentShader);
+
+            GL.LinkProgram(_handle);
+
+            string infoLogLink = GL.GetShaderInfoLog(VertexShader);
+            if (infoLogLink != System.String.Empty)
+                System.Console.WriteLine(infoLogLink);
+
+            GL.DetachShader(_handle, VertexShader);
+            GL.DetachShader(_handle, FragmentShader);
+            GL.DeleteShader(FragmentShader);
+            GL.DeleteShader(VertexShader);
+        }
+
+
+        public void Use()
+        {
+            GL.UseProgram(_handle);
+        }
+
+
+        public void SetInt(string name, int value)
+        {
+            int location = GL.GetUniformLocation(_handle, name);
+
+            if (location == -1)
+            {
+                throw new ArgumentException("uniform name not found");
+            }
+
+            GL.Uniform1(location, value);
+        }
+
+        
+        public void SetMatrix4(string name, Matrix4 matrix)
+        {
+            int location = GL.GetUniformLocation(_handle, name);
+
+            if (location == -1)
+            {
+                throw new ArgumentException("uniform name not found");
+            }
+
+            GL.UniformMatrix4(location, true, ref matrix);
+        }
+
+
+        public int GetAttribLocation(string attribName)
+        {
+            return GL.GetAttribLocation(_handle, attribName);
+        }
+
+
+        private string LoadSource(string path)
+        {
+            string readContents;
+
+            using (StreamReader streamReader = new StreamReader(path, Encoding.UTF8))
+            {
+                readContents = streamReader.ReadToEnd();
+            }
+
+            return readContents;
+        }
+
+
+        private bool _disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                GL.DeleteProgram(_handle);
+
+                _disposedValue = true;
+            }
+        }
+
+        ~Shader()
+        {
+            GL.DeleteProgram(_handle);
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Openglapp/Texture.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenglApp.Imageutils;
+using OpenTK.Graphics.OpenGL4;
+
+namespace OpenglApp
+{
+    public class Texture : IDisposable
+    {
+        int _handle;
+
+        public Texture(string path)
+        {
+            _handle = GL.GenTexture();
+            Use();
+
+            var image = ImageUtil.GetImage(path);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, image.Width, image.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, image.Data);
+
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+        }
+
+        public void Use(TextureUnit unit = TextureUnit.Texture0)
+        {
+            GL.ActiveTexture(unit);
+            GL.BindTexture(TextureTarget.Texture2D, _handle);
+        }
+
+        private bool _disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                GL.DeleteProgram(_handle);
+
+                _disposedValue = true;
+            }
+        }
+
+        ~Texture()
+        {
+            GL.DeleteProgram(_handle);
+        }
+
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
