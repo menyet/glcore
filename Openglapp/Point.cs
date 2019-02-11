c2c9f2490db7da5418232ae913a2e46ef5598b73
Unknown <Andrej.Mandli@erni.sk>
2019-02-11 11:31:59 +0100

    Add street object

---
+++ Openglapp/Point.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenglApp
+{
+    public struct Point
+    {
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public float Z { get; set; }
+    }
+}
